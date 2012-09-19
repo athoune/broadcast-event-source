7273b740e5c9ac57177e15a6e0b96d4fc98c29f6
Mathieu Lecarme <mathieu@garambrogne.net>
2012-09-19 23:47:41 +0200

    With some locks.

--- pubsub.go
+++ pubsub.go
@@ -1,6 +1,11 @@
 package main
+/* Stolen from https://github.com/bitly/go-notify/blob/master/notify.go */
+import (
+	"sync"
+)
 
 var clientCount = 0
+var clientsLock sync.RWMutex
 var clients map[int]chan string = make(map[int]chan string)
 
 type client struct {
@@ -9,6 +14,8 @@ type client struct {
 }
 
 func Suscribe() client {
+	clientsLock.Lock()
+	defer clientsLock.Unlock()
 	clientCount += 1
 	ch := make(chan string)
 	clients[clientCount] = ch
@@ -17,11 +24,15 @@ func Suscribe() client {
 }
 
 func (c *client) Leave() {
+	clientsLock.Lock()
+	defer clientsLock.Unlock()
 	delete(clients, c.id)
 	close(c.channel)
 }
 
 func Publish(m string) {
+	clientsLock.RLock()
+	defer clientsLock.RUnlock()
 	for i := range clients {
 		clients[i] <- m
 	}
